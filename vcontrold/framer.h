c4928dc2104c5bdef0e682ee8b6760d07a3ca3f2
Thomas Kunze <thomas.kunze@gmx.com>
2016-07-25 03:48:03 +0200

    cleanup, less warnings, use pthread

--- vcontrold/framer.h
+++ vcontrold/framer.h
@@ -8,16 +8,16 @@
  * 2013-01-31 vheat
  */
 
-/*
- * Main indentification of P300 Protocol is about leadin 0x41
- */
+ /*
+  * Main indentification of P300 Protocol is about leadin 0x41
+  */
 
 #define FRAMER_ERROR	0
 #define FRAMER_SUCCESS	1
 
-int framer_send(int fd,char *s_buf, int len);
+int framer_send(int fd, char *s_buf, char len);
 
-int framer_waitfor(int fd, char *w_buf,int w_len);
+int framer_waitfor(int fd, char *w_buf, int w_len);
 
 int framer_receive(int fd, char *r_buf, int r_len, unsigned long *petime);
 
